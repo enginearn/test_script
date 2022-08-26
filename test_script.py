05f5cc76c5950cb2c48d08a04c68826e81fed200
enginearn <enginyaaaaan@gmail.com>
2022-08-27 02:41:36 +0900

    Done!

---
+++ test_script.py
@@ -0,0 +1,46 @@
+#!/bin/bash/env python3
+
+import csv
+import glob
+import re
+
+file_name = glob.glob("*.csv")[0]
+with open(file_name, "r") as file:
+    reader = csv.DictReader(file)
+    # fieldnames = next(reader)
+    # print(fieldnames)
+
+    results = {}
+
+    for row in reader:
+        if not row["COL_2"] in results:
+            if row["COL_3"] == "開始":
+                results[row["COL_2"]] = {row["COL_3"]: row["COL_1"]}
+            elif row["COL_3"] == "終了":
+                results[row["COL_2"]] = {row["COL_3"]: row["COL_1"]}
+            elif row["COL_3"] == "正常":
+                results[row["COL_2"]] = {row["COL_3"]: row["COL_1"]}
+            elif "コマンド" in row["COL_3"]:
+                res = re.match("(^コマンド\([0-9]*2).*\)", row["COL_3"])
+                if res:
+                    results[row["COL_2"]] = {row["COL_3"]: row["COL_1"]}
+            else:
+                raise ValueError(f"unknown COL_3: {row['COL_3']}")
+        else:
+            if row["COL_3"] == "開始" and row["COL_2"] in results:
+                id = row["COL_2"]
+                results[id][row["COL_3"]] = row["COL_1"]
+            elif row["COL_3"] == "終了" and row["COL_2"] in results:
+                id = row["COL_2"]
+                results[row["COL_2"]][row["COL_3"]] = row["COL_1"]
+            elif row["COL_3"] == "正常" and row["COL_2"] not in results:
+                id = row["COL_2"]
+                results[id] = {row["COL_3"]: row["COL_1"]}
+            elif row["COL_2"] in results:
+                id = row["COL_2"]
+                res = re.match("(^コマンド\([0-9]*2).*\)", row["COL_3"])
+                if res:
+                    results[id][row["COL_3"]] = row["COL_1"]
+            else:
+                raise ValueError(f"unknown COL_2: {row['COL_2']}")
+    print(results)
